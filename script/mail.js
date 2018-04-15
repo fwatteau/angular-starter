e529346b3c6465b949197865d5210e329c161ea5
Kef <f.watteau@gmail.com>
2018-04-15 12:12:23 +0200

    - EVOL : Envoie de mail

--- script/mail.js
+++ script/mail.js
@@ -2,6 +2,7 @@
 // Generate test SMTP service account from ethereal.email
 exports.handler = function(event, context, callback) {
     const nodemailer = require('nodemailer');
+    const nodemailer2 = require('../node_modules/nodemailer');
     const transporter = nodemailer.createTransport({
         host: process.env.MAIL_SMTP,
         port: 587,
